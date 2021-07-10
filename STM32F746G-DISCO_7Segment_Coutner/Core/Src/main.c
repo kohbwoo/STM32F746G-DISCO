3569655d755ffe553688d196b56bc37db6589e76
kohbwoo <kohbwoo@gmail.com>
2021-07-10 20:26:06 +0900

    .

--- STM32F746G-DISCO_7Segment_Coutner/Core/Src/main.c
+++ STM32F746G-DISCO_7Segment_Coutner/Core/Src/main.c
@@ -1,16 +1,14 @@
+/* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-/* Private function prototypes -----------------------------------------------*/
-	/*
 
-	Segment Info
-
-	{0,0,0,1},
-	{0,0,1,0},
-	{0,1,0,0},
-	{1,0,0,0}
-
-	    A
+/*
+ Segment Info
+ {0,0,0,1},
+ {0,0,1,0},
+ {0,1,0,0},
+ {1,0,0,0}
+ 	 	A
 	   ---
 	F |   | B
 	  | G |
@@ -19,34 +17,33 @@
 	  |   |
 	   ---
 	    D
+	 {1,1,0,0,0,0,0,0}, //0
+	 {1,1,1,1,1,0,0,1}, //1
+	 {1,0,1,0,0,1,0,0}, //2
+	 {1,0,1,1,0,0,0,0}, //3
+	 {1,0,0,1,1,0,0,1}, //4
+	 {1,0,0,1,0,0,1,0}, //5
+	 {1,0,0,0,0,0,1,0}, //6
+	 {1,1,0,1,1,0,0,0}, //7
+	 {1,0,0,0,0,0,0,0}, //8
+	 {1,0,0,1,1,0,0,0}  //9
+    DOT,G,F,E,D,C,B,A
+ Pin Info
+ pinA = 2 = PG6  = (GPIOG, GPIO_PIN_6)
+ pinB = 3 = PB4  = (GPIOB, GPIO_PIN_4)
+ pinC = 4 = PG7  = (GPIOG, GPIO_PIN_7)
+ pinD = 5 = PI0  = (GPIOI, GPIO_PIN_0)
+ pinE = 6 = PH6  = (GPIOH, GPIO_PIN_6)
+ pinF = 7 = PI3  = (GPIOI, GPIO_PIN_3)
+ pinG = 8 = PI2  = (GPIOI, GPIO_PIN_2)
+ D1 = 9   = PA15 = (GPIOA, GPIO_PIN_15)
+ D2 = 10  = PA8  = (GPIOA, GPIO_PIN_8)
+ D3 = 11  = PB15 = (GPIOB, GPIO_PIN_15)
+ D4 = 12  = PB14 = (GPIOB, GPIO_PIN_14)
+ */
 
-	{1,1,0,0,0,0,0,0}, //0
-	{1,1,1,1,1,0,0,1}, //1
-	{1,0,1,0,0,1,0,0}, //2
-	{1,0,1,1,0,0,0,0}, //3
-	{1,0,0,1,1,0,0,1}, //4
-	{1,0,0,1,0,0,1,0}, //5
-	{1,0,0,0,0,0,1,0}, //6
-	{1,1,0,1,1,0,0,0}, //7
-	{1,0,0,0,0,0,0,0}, //8
-	{1,0,0,1,1,0,0,0}  //9
-   DOT,G,F,E,D,C,B,A
+int8_t Min_Up = 0;
 
-	Pin Info
-	pinA = 2 = PG6  = (GPIOG, GPIO_PIN_6)
-	pinB = 3 = PB4  = (GPIOB, GPIO_PIN_4)
-	pinC = 4 = PG7  = (GPIOG, GPIO_PIN_7)
-	pinD = 5 = PI0  = (GPIOI, GPIO_PIN_0)
-	pinE = 6 = PH6  = (GPIOH, GPIO_PIN_6)
-	pinF = 7 = PI3  = (GPIOI, GPIO_PIN_3)
-	pinG = 8 = PI2  = (GPIOI, GPIO_PIN_2)
-	D1 = 9   = PA15 = (GPIOA, GPIO_PIN_15)
-	D2 = 10  = PA8  = (GPIOA, GPIO_PIN_8)
-	D3 = 11  = PB15 = (GPIOB, GPIO_PIN_15)
-	D4 = 12  = PB14 = (GPIOB, GPIO_PIN_14)
-
-
-	*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 unsigned char Segment_Test(unsigned short delaytime);
@@ -54,29 +51,28 @@ unsigned char Segment_Select(unsigned char SegmentNum, unsigned char PrintNum);
 unsigned char Num_Select(unsigned char PrintNumx16);
 
 int main(void) {
-	/* Enable I-Cache---------------------------------------------------------*/
-	SCB_EnableICache();
-	/* Enable D-Cache---------------------------------------------------------*/
-	SCB_EnableDCache();
-	/* MCU Configuration-------------------------------------------------------*/
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 	HAL_Init();
-	/* Configure the system clock */
 	SystemClock_Config();
-	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
-	unsigned char List_Of_Segments[4] = {0x01, 0x02, 0x04, 0x08};
-	unsigned char List_Of_Segment_Info[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xD8, 0x80, 0x98};
+	unsigned char List_Of_Segments[4] = { 0x01, 0x02, 0x04, 0x08 };
+	unsigned char List_Of_Segment_Info[10] = { 0xC0, 0xF9, 0xA4, 0xB0, 0x99,
+			0x92, 0x82, 0xD8, 0x80, 0x98 };
 	unsigned short delaytime = 1;
-	unsigned char addr[4] = {0,0,0,0};
-	unsigned short input = 5673; //인풋//////////////////////////////////////////////////////////////////////////
-	addr[0] = input / 1000;
-	addr[1] = input % 1000 / 100;
-	addr[2] = input % 100 / 10;
-	addr[3] = input % 10;
-	/* Infinite loop */
+	unsigned char addr[4] = { 0, 0, 0, 0 };
+	unsigned short input = 1255; //인풋//////////////////////////////////////////////////////////////////////////
+//	addr[0] = input / 1000;
+//	addr[1] = input % 1000 / 100;
+//	addr[2] = input % 100 / 10;
+//	addr[3] = input % 10;
 
 	while (1) {
+		addr[0] = input / 1000;
+		addr[1] = input % 1000 / 100;
+		addr[2] = input % 100 / 10;
+		addr[3] = input % 10;
+
+		Min_Up = 0;
+
 		Segment_Select(List_Of_Segments[0], List_Of_Segment_Info[addr[0]]);
 		HAL_Delay(delaytime);
 		Segment_Select(List_Of_Segments[1], List_Of_Segment_Info[addr[1]]);
@@ -85,81 +81,73 @@ int main(void) {
 		HAL_Delay(delaytime);
 		Segment_Select(List_Of_Segments[3], List_Of_Segment_Info[addr[3]]);
 		HAL_Delay(delaytime);
+		input = input + Min_Up;
+
 	}
-	/* USER CODE END 3 */
+
 }
-unsigned char Num_Select(unsigned char PrintNumx16){
-	if(PrintNumx16 & 0x40){
+
+unsigned char Num_Select(unsigned char PrintNumx16) {
+	if (PrintNumx16 & 0x40) {
 		HAL_GPIO_WritePin(GPIOI, GPIO_PIN_2, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOI, GPIO_PIN_2, 0);
 	}
-	if(PrintNumx16 & 0x20){
+	if (PrintNumx16 & 0x20) {
 		HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3, 0);
 	}
-	if(PrintNumx16 & 0x10){
+	if (PrintNumx16 & 0x10) {
 		HAL_GPIO_WritePin(GPIOH, GPIO_PIN_6, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOH, GPIO_PIN_6, 0);
 	}
-	if(PrintNumx16 & 0x08){
+	if (PrintNumx16 & 0x08) {
 		HAL_GPIO_WritePin(GPIOI, GPIO_PIN_0, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOI, GPIO_PIN_0, 0);
 	}
-	if(PrintNumx16 & 0x04){
+	if (PrintNumx16 & 0x04) {
 		HAL_GPIO_WritePin(GPIOG, GPIO_PIN_7, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOG, GPIO_PIN_7, 0);
 	}
-	if(PrintNumx16 & 0x02){
+	if (PrintNumx16 & 0x02) {
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 0);
 	}
-	if(PrintNumx16 & 0x01){
+	if (PrintNumx16 & 0x01) {
 		HAL_GPIO_WritePin(GPIOG, GPIO_PIN_6, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOG, GPIO_PIN_6, 0);
 	}
 	return 0;
 }
 
-unsigned char Segment_Select(unsigned char SegmentNumx16, unsigned char PrintNumx16) {
+unsigned char Segment_Select(unsigned char SegmentNumx16,
+		unsigned char PrintNumx16) {
 	//출력할 세그먼트 결정
 
-
-	if(SegmentNumx16 & 0x08){
+	if (SegmentNumx16 & 0x08) {
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 0);
 	}
-	if(SegmentNumx16 & 0x04){
-			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, 1);
-	}
-	else{
+	if (SegmentNumx16 & 0x04) {
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, 1);
+	} else {
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, 0);
 	}
-	if(SegmentNumx16 & 0x02){
+	if (SegmentNumx16 & 0x02) {
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, 0);
 	}
-	if(SegmentNumx16 & 0x01){
+	if (SegmentNumx16 & 0x01) {
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, 1);
-	}
-	else{
+	} else {
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, 0);
 	}
 	Num_Select(PrintNumx16);
@@ -168,6 +156,7 @@ unsigned char Segment_Select(unsigned char SegmentNumx16, unsigned char PrintNum
 }
 
 unsigned char Segment_Test(unsigned short delaytime) {
+//	unsigned short delaytime = 10;
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, 1);
 	HAL_Delay(delaytime);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, 0);
@@ -182,11 +171,7 @@ unsigned char Segment_Test(unsigned short delaytime) {
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 0);
 	return 0;
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/**
- * @brief System Clock Configuration
- * @retval None
- */
+
 void SystemClock_Config(void) {
 	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
 	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
@@ -251,7 +236,7 @@ static void MX_GPIO_Init(void) {
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15 | GPIO_PIN_8, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0,
+	HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1,
 			GPIO_PIN_RESET);
 
 	/*Configure GPIO pin Output Level */
@@ -259,6 +244,10 @@ static void MX_GPIO_Init(void) {
 
 	/*Configure GPIO pin Output Level */
 	HAL_GPIO_WritePin(GPIOH, GPIO_PIN_6, GPIO_PIN_RESET);
+
+
+
+
 
 	/*Configure GPIO pins : PB4 PB14 PB15 */
 	GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_14 | GPIO_PIN_15;
@@ -274,11 +263,17 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-	/*Configure GPIO pins : PI3 PI2 PI1 PI0 */
-	GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0;
+	/*Configure GPIO pins : PI3 PI2 PI1 PI0 PI0프로젝트 설정이 아닌 핀 추가로 오류 가능성 있음*/
+	GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1|GPIO_PIN_0;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
+
+	/*Configure GPIO pin : PI11 */
+	GPIO_InitStruct.Pin = GPIO_PIN_11;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
 
 	/*Configure GPIO pins : PG7 PG6 */
@@ -288,12 +283,25 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
 
+	/*Configure GPIO pin : PA0 */
+	GPIO_InitStruct.Pin = GPIO_PIN_0;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 	/*Configure GPIO pin : PH6 */
 	GPIO_InitStruct.Pin = GPIO_PIN_6;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+
+	/* EXTI interrupt init*/
+	HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+	HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
 
---
+++ STM32F746G-DISCO_7Segment_Coutner/Core/Src/stm32f7xx_it.c
@@ -0,0 +1,225 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32f7xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f7xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+extern int8_t Min_Up;/////////////////////////////////////////////////
+
+
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+  while (1)
+  {
+  }
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+
+  /* USER CODE END MemoryManagement_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Pre-fetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
+  /* USER CODE END BusFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
+  /* USER CODE END UsageFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32F7xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f7xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles RCC global interrupt.
+  */
+void RCC_IRQHandler(void)
+{
+  /* USER CODE BEGIN RCC_IRQn 0 */
+
+  /* USER CODE END RCC_IRQn 0 */
+  /* USER CODE BEGIN RCC_IRQn 1 */
+
+  /* USER CODE END RCC_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line0 interrupt.
+  */
+void EXTI0_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI0_IRQn 0 */
+
+  /* USER CODE END EXTI0_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+  /* USER CODE BEGIN EXTI0_IRQn 1 */
+
+  /* USER CODE END EXTI0_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[15:10] interrupts.
+  */
+void EXTI15_10_IRQHandler(void) {
+	if (HAL_GPIO_ReadPin(GPIOI, GPIO_PIN_11)){
+		Min_Up = 1;
+	}
+	else{
+		Min_Up = 0;
+	}
+//	while ((HAL_GPIO_ReadPin(GPIOI, GPIO_PIN_11))) {
+//		if (HAL_GPIO_ReadPin(GPIOI, GPIO_PIN_11)) {
+//			Min_Up = 1;
+//			return 0;
+//		}
+//		else{
+//			Min_Up = 0;
+//			return 0;
+//		}
+//	}
+
+
+  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
+
+
+  /* USER CODE END EXTI15_10_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_11);
+  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
+
+  /* USER CODE END EXTI15_10_IRQn 1 */
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
